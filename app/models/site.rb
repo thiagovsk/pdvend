7ad15b8153e008c48851de97cb1606a6d5089263
Thiago Ribeiro <thiagitosouza@gmail.com>
2016-10-28 10:22:52 -0200

    Generate tags with nokogiri by url

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,4 +1,15 @@
+require 'open-uri'
 class Site < ActiveRecord::Base
   has_many :tags
+
   validates :url, presence: true, format: { with: URI.regexp }
+
+  after_commit :generate_tags
+
+  def generate_tags
+    page = Nokogiri::HTML(open(url))
+    page.search('h1, h2, h3').map do |node|
+      Tag.create(site_id: id, name: node.name, content: node.text)
+    end
+  end
 end
